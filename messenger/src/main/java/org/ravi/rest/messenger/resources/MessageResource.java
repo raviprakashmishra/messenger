985d86c72f3571c0807bbd1dd0573cefc5213a31
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-06-30 22:50:05 -0700

    updated message resource

--- messenger/src/main/java/org/ravi/rest/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/ravi/rest/messenger/resources/MessageResource.java
@@ -1,17 +1,23 @@
 package org.ravi.rest.messenger.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.ravi.rest.messenger.model.Message;
+import org.ravi.rest.messenger.service.MessageService;
+
 @Path("/messages")
 public class MessageResource {
+	MessageService service = new MessageService();
 
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getMessages() {
-		return "Hello World!";
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages() {
+		return service.getAllMessages();
 	}
 	
 	
