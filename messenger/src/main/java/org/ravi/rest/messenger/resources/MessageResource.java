a931a017a71c5e6d167624cd816f984100dee32f
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-07-04 00:23:11 -0700

    added new message creation capability

--- messenger/src/main/java/org/ravi/rest/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/ravi/rest/messenger/resources/MessageResource.java
@@ -2,7 +2,10 @@ package org.ravi.rest.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,16 +20,23 @@ public class MessageResource {
 	
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return service.geAlltMessages();
 	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long messageId){
 		return service.getMessage(messageId);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return service.addMessage(message);
 	}
 	
 	
