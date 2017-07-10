79c220039cb38a382d4b515a6cf0eacb29990c93
rmishra <rmishra@RMISHRA01-LT.vocera.local>
2017-07-09 17:09:28 -0700

    adding profile resource

---
+++ messenger/src/main/java/org/ravi/rest/messenger/service/ProfileService.java
@@ -0,0 +1,62 @@
+package org.ravi.rest.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.ravi.rest.messenger.database.Database;
+import org.ravi.rest.messenger.model.Message;
+import org.ravi.rest.messenger.model.Profile;
+
+public class ProfileService {
+
+	
+	Map<String, Profile> profiles = Database.getProfiles();
+	
+	public ProfileService() {
+		// TODO Auto-generated constructor stub
+		profiles.put("ravi",new Profile(1L,"ravi","Ravi","Mishra" ));
+		//profiles.put(2L,new Message("2Msg","ravi",2L));
+	}
+	
+	
+	public List<Profile> geAllProfiles() {
+		return new ArrayList<>(profiles.values());
+	}
+	
+	public Profile getProfile(String profileName){
+		return profiles.get(profileName);
+	}
+	
+	public Profile addMessage(Profile profile){
+		if (profile == null) return null;
+		profile.setId(profiles.size()+1);
+		profiles.put(profile.getProfileName(),profile);
+		return profile;		
+		
+	}
+	
+	public Profile updateMessage(Profile profile){
+		if (profile.getProfileName().isEmpty()) return null;
+		
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public void removeProfile(String profileName){
+		profiles.remove(profileName);
+		
+	}
+	
+	
+	/*public List<Message> getAllMessages(){
+		Message m1 = new Message("Hello world1", "ravi", 1L, new Date());
+		Message m2 = new Message("Hello world123", "ravi", 2L, new Date());
+		 List<Message> list = new ArrayList<>();
+		 list.add(m1);
+		 list.add(m2);
+		return list;
+	}*/
+
+
+}
